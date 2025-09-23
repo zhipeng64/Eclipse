9b0eba3eb5f3a33fc0b9f35bcf50baccb2180db0
zhipeng64 <zhipenglin64@gmail.com>
2025-09-23 18:32:21 -0400

    Added Dockerfiles to dockerize an nginx reverse proxy and the backend. Nginx container does SSL termination and communincates with the backend via http.

--- src/backend/utils/socket.js
+++ src/backend/utils/socket.js
@@ -1,5 +1,5 @@
 import { parseCookies } from "./cookie.js";
-import authService from "../services/authService.js";
+import authService from "../services/AuthService.js";
 const getTokens = async (cookieString) => {
   // A connecting client socket must pass authentication checks
   const { jwt, refreshToken } = parseCookies(cookieString);
